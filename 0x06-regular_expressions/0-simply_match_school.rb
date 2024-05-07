deab8856604db313b249b67486a2cebf8e2d0a83
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-05-07 06:41:46 -0700

    Simply matching school

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,23 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Usage: ruby script_name.rb <string>"
-  exit
-end
-
-# Get the argument
-input_string = ARGV[0]
-
-# Define the regular expression pattern
-pattern = /\bSchool\b/
-
-# Perform the regular expression matching
-matches = input_string.scan(pattern)
-
-# Print the matches
-if matches.empty?
-  puts "No matches found."
-else
-  puts "Matches found: #{matches.join(', ')}"
-end
+puts ARGV[0].scan(/School[abc]/).join
