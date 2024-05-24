6b38b15b1a0766148f697282b94a929b0631c5fb
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-05-24 06:46:45 -0700

    Score too low

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,5 @@
+-- A script that removes all the records with a score <= 5 in second_table
+
+-- SQL query that removes all the records witha a score <= 5 in second_table
+DELETE FROM second_table
+WHERE score <= 5;
