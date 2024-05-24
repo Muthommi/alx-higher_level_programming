7ab585dbd87870fe6a97664ca3e900f8c7d68359
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-05-24 02:59:43 -0700

    First table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,11 +1,5 @@
 -- This script creates a table called first_table in the specified database
 
--- SQL query to create the database if it does not exist
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
--- Use the specified database
-USE hbtn_0c_0;
-
 -- SQL query to create the table if it does not exist
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
