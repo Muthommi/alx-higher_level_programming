2ce51ef2abadba5f0a16c3979e67557247f16cfe
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-05-27 04:22:32 -0700

    Always a name

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,10 @@
+-- A script that creates a table
+
+-- Use the database
+USE hbtn_0d_2
+
+-- SQL query to create a table
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
