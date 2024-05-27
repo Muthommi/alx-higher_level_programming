c2b7d5290c147635a9617b70095fa366f9d96657
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-05-27 02:30:40 -0700

    Root user

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- This is a script that lists all the privileges of MySQL users
+
+-- SQL query that lists all the privileges of MySQL users
+SHOW GRANTS FOR '{USER_1}'@'{LOCALHOST}';
+SHOW GRANTS FOR '{USER_2}'@'{LOCALHOST}'; 
