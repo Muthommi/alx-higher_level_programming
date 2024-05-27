94a0cc969e4b9bd2ddb649aa6c59b6f66e8935b7
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-05-27 05:22:13 -0700

    Cities of California

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,13 @@
+-- A script that lists all the cities
+
+-- SQL query to show what database to use
+USE hbtn_0d_usa
+
+-- SQL query to find the id of california
+SET @california_id := (SELECT id FROM states WHERE name = 'California');
+
+-- SQL query to list all the cities
+SELECT id, name
+FROM cities
+WHERE state_id = @california_id
+ORDER BY id ASC;
