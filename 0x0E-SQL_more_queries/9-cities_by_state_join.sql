0977ac907b5d7eb1f2438642aa65837201e71bca
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-05-27 05:33:34 -0700

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,8 @@
 -- A script that lists all the cities in hbtn_0d_2 database
 
+-- Use the database
+USE hbtn_0d_usa;
+
 -- SQL query that lists all the cities in the database
 SELECT cities.id, cities.name, states.name AS state_name
 FROM cities
