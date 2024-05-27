67bc4802cd69f146e88532823ca4ca00d645c269
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-05-27 03:08:54 -0700

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- A script that creates the database and a user
 
 -- SQL query to create a database 
-CREATE DATABASE IF NOT EXISTS hbdtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- SQL query to create a user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
