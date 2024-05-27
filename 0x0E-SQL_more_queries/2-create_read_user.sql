e9048b31819379da006bd348a7cb0c2fdc62ca30
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-05-27 03:05:33 -0700

    Read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,13 @@
+-- A script that creates the database and a user
+
+-- SQL query to create a database 
+CREATE DATABASE IF NOT EXISTS hbdtn_0d_2;
+
+-- SQL query to create a user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- SQL query to grant privileges
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges
+FLUSH PRIVILEGES;
