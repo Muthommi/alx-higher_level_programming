22a48193f7da5499e39ec8e5b8a23fa69914afe7
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-06-17 23:54:35 -0700

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 const arg = process.argv[2];
 const x = parseInt(arg);
 
-if (isNaN(x) || x <= 0) {
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
-} else {
+} else if (x > 0) {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
