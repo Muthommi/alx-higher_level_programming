4d2ed28da54d2529acd5e38423d4cac43c31b2d6
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-07-11 00:37:42 -0700

    Only status code

---
+++ 0x10-python-network_0/100-status_code.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# A script that sends a request to a URL passed as an argument
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+curl -s -o /dev/null -w "%{http_code}" "$1"
